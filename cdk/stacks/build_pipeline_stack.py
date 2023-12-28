7ccd8cd0bd747e0b5271b4a141908b649693cfcd
Justin Hendrickson <justin.hendrickson@thebernardgroup.com>
2023-12-28 16:35:55 -0600

    feat: add prod pipeline and stage

--- cdk/stacks/build_pipeline_stack.py
+++ cdk/stacks/build_pipeline_stack.py
@@ -6,6 +6,100 @@ from aws_cdk import pipelines, aws_iam, aws_codestarconnections, aws_lambda
 import cdk.stages.dev_stage
 
 
+class BuildProdPipelineStack(aws_cdk.Stack):
+    def __init__(
+        self,
+        scope: constructs.Construct,
+        id: str,
+        *,
+        namer: tbg_cdk.IResourceNamer,
+        **kwargs
+    ):
+        super().__init__(scope=scope, id=id, **kwargs)
+
+        self.github_connection = aws_codestarconnections.CfnConnection(
+            scope=self,
+            id="AlarmNotifierGitHubConnection",
+            connection_name="AlarmNotifierGitHubConnection",
+            provider_type="GitHub",
+        )
+
+        self.dev_pipeline = pipelines.CodePipeline(
+            scope=self,
+            id="ProdPipeline",
+            pipeline_name=namer.get_name("AwsAlarmNotifierProdPipeline"),
+            synth=pipelines.ShellStep(
+                id="Synth",
+                commands=[
+                    "export ASDF_DIR=~/.asdf",
+                    "git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1",
+                    ". $ASDF_DIR/asdf.sh",
+                    "asdf plugin add nodejs",
+                    "asdf plugin add poetry",
+                    "asdf plugin add python",
+                    "asdf plugin add awscli",
+                    "asdf install",
+                    "npm i",
+                    "poetry config http-basic.tbg aws $(aws codeartifact get-authorization-token --duration-seconds 3600 --domain tbg --domain-owner 538493872512 --query authorizationToken --output text)",
+                    "poetry install --no-root --without=dev",
+                    "npx cdk --context codeartifact_authorization_token=`aws codeartifact get-authorization-token --duration-seconds 3600 --domain tbg --domain-owner 538493872512 --query authorizationToken --output text` synth",
+                ],
+                input=pipelines.CodePipelineSource.connection(
+                    repo_string="collaterate/aws-cloudwatch-alarm-notifier",
+                    branch="main",
+                    connection_arn=self.github_connection.attr_connection_arn,
+                    action_name=namer.get_name("GitHub"),
+                ),
+            ),
+            synth_code_build_defaults=pipelines.CodeBuildOptions(
+                role_policy=[
+                    aws_iam.PolicyStatement(
+                        actions=[
+                            "codeartifact:GetAuthorizationToken",
+                            "codeartifact:GetRepositoryEndpoint",
+                            "codeartifact:ReadFromRepository",
+                        ],
+                        effect=aws_iam.Effect.ALLOW,
+                        resources=[
+                            self.node.try_get_context("tbg-codeartifact-domain-arn")
+                        ],
+                    ),
+                    aws_iam.PolicyStatement(
+                        actions=[
+                            "codeartifact:GetRepositoryEndpoint",
+                            "codeartifact:ReadFromRepository",
+                        ],
+                        effect=aws_iam.Effect.ALLOW,
+                        resources=[
+                            self.node.try_get_context(
+                                "tbg-codeartifact-python-repository-arn"
+                            )
+                        ],
+                    ),
+                    aws_iam.PolicyStatement(
+                        actions=["sts:GetServiceBearerToken"],
+                        conditions={
+                            "StringEquals": {
+                                "sts:AWSServiceName": "codeartifact.amazonaws.com"
+                            }
+                        },
+                        effect=aws_iam.Effect.ALLOW,
+                        resources=["*"],
+                    ),
+                ]
+            ),
+        )
+
+        self.dev_pipeline.add_stage(
+            stage=cdk.stages.prod_stage.ProdStage(
+                scope=self,
+                id="ProdStage",
+                env=aws_cdk.Environment(account="538493872512", region="us-east-1"),
+                stage_name=namer.get_name("ProdStage"),
+            ),
+        )
+
+
 class BuildDevPipelineStack(aws_cdk.Stack):
     def __init__(
         self,
---
+++ cdk/stages/prod_stage.py
@@ -0,0 +1,121 @@
+import aws_cdk
+import cdk_nag
+import constructs
+import tbg_cdk
+from aws_cdk import aws_iam
+
+import cdk.stacks.application_stack
+
+
+class ProdStage(aws_cdk.Stage):
+    def __init__(self, scope: constructs.Construct, id: str, **kwargs):
+        super().__init__(scope=scope, id=id, **kwargs)
+
+        namer = tbg_cdk.ResourceNamer(["Prod", "Prv", "UE1"])
+
+        self.stack = cdk.stacks.application_stack.ApplicationStack(
+            scope=self,
+            id="AlarmNotifier",
+            namer=namer.with_prefix("AlarmNotifier"),
+            sentry_dsn_secret_name="/Sentry/AlarmNotifier/Dsn",
+            sentry_env="prod",
+            slack_alarm_notifier_oauth_token_secret_name="/Slack/AWSCloudWatchAlarmNotifier/BotUserOAuthToken",
+            stack_name=namer.get_name("AlarmNotifier"),
+        )
+
+        self.permissions_boundary = aws_iam.ManagedPolicy(
+            scope=self.stack,
+            id="PermissionsBoundary",
+            description="Permissions boundary for the alarm notifier stack",
+            managed_policy_name=namer.get_name("PermissionsBoundary"),
+            statements=[
+                aws_iam.PolicyStatement(actions=["*"], resources=["*"]),
+                aws_iam.PolicyStatement(
+                    actions=[
+                        "iam:CreateRole",
+                        "iam:CreateUser",
+                        "iam:PutRolePermissionsBoundary",
+                        "iam:PutUserPermissionsBoundary",
+                    ],
+                    conditions={
+                        "ArnNotEquals": {
+                            "iam:PermissionsBoundary": aws_cdk.Arn.format(
+                                components=aws_cdk.ArnComponents(
+                                    resource="policy",
+                                    service="iam",
+                                    resource_name=namer.get_name("PermissionsBoundary"),
+                                ),
+                                stack=self.stack,
+                            )
+                        }
+                    },
+                    effect=aws_iam.Effect.DENY,
+                    resources=["*"],
+                ),
+                aws_iam.PolicyStatement(
+                    actions=[
+                        "iam:CreatePolicyVersion",
+                        "iam:DeletePolicy",
+                        "iam:DeletePolicyVersion",
+                        "iam:SetDefaultPolicyVersion",
+                    ],
+                    effect=aws_iam.Effect.DENY,
+                    resources=[
+                        aws_cdk.Arn.format(
+                            components=aws_cdk.ArnComponents(
+                                resource="policy",
+                                service="iam",
+                                region="",
+                                resource_name=namer.get_name("PermissionsBoundary"),
+                            ),
+                            stack=self.stack,
+                        )
+                    ],
+                ),
+                aws_iam.PolicyStatement(
+                    actions=[
+                        "iam:DeleteRolePermissionsBoundary",
+                        "iam:DeleteUserPermissionsBoundary",
+                    ],
+                    effect=aws_iam.Effect.DENY,
+                    resources=["*"],
+                ),
+                aws_iam.PolicyStatement(
+                    not_actions=[
+                        "ec2:CreateNetworkInterface",
+                        "ec2:DescribeNetworkInterfaces",
+                        "ec2:DeleteNetworkInterface",
+                        "ec2:AssignPrivateIpAddresses",
+                        "ec2:UnassignPrivateIpAddresses",
+                    ],
+                    conditions={
+                        "StringNotEquals": {
+                            "aws:ResourceTag/ApplicationName": "Alarm Notifier",
+                            "aws:ResourceTag/Environment": "Development",
+                        }
+                    },
+                    effect=aws_iam.Effect.DENY,
+                    resources=["*"],
+                ),
+                aws_iam.PolicyStatement(
+                    actions=["ec2:DeleteTags", "tag:UntagResources"],
+                    effect=aws_iam.Effect.DENY,
+                    resources=["*"],
+                ),
+            ],
+        )
+
+        cdk_nag.NagSuppressions.add_resource_suppressions(
+            construct=self.permissions_boundary,
+            suppressions=[
+                cdk_nag.NagPackSuppression(
+                    applies_to=["Action::*", "Resource::*"],
+                    id="AwsSolutions-IAM5",
+                    reason="Permission boundaries are allowed to use wildcards",
+                )
+            ],
+        )
+
+        aws_iam.PermissionsBoundary.of(self.stack).apply(self.permissions_boundary)
+
+        aws_cdk.Tags.of(self).add("Environment", "Production")
