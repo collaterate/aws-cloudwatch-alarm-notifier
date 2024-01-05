d0c21f805c024d15080ef6dfa53efbc4644b89a6
Justin Hendrickson <justin.hendrickson@thebernardgroup.com>
2024-01-05 16:10:48 -0600

    fix: temporarily remove the prod pipeline

--- cdk/app.py
+++ cdk/app.py
@@ -14,11 +14,11 @@ cdk.stacks.build_pipeline_stack.BuildDevPipelineStack(
     namer=tbg_cdk.ResourceNamer(["Dev", "Prv", "UE1", "AlarmNotifierPipeline"]),
 )
 
-cdk.stacks.build_pipeline_stack.BuildProdPipelineStack(
-    scope=app,
-    id="ProdAlarmNotifierPipeline",
-    env=aws_cdk.Environment(account="538493872512", region="us-east-1"),
-    namer=tbg_cdk.ResourceNamer(["Prod", "Prv", "UE1", "AlarmNotifierPipeline"]),
-)
+# cdk.stacks.build_pipeline_stack.BuildProdPipelineStack(
+#     scope=app,
+#     id="ProdAlarmNotifierPipeline",
+#     env=aws_cdk.Environment(account="538493872512", region="us-east-1"),
+#     namer=tbg_cdk.ResourceNamer(["Prod", "Prv", "UE1", "AlarmNotifierPipeline"]),
+# )
 
 app.synth()
